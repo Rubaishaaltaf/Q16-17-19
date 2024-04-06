d9cc6f47a1b381d909a6318e4bbae0ea80fad711
Rubaishaaltaf <160398911+Rubaishaaltaf@users.noreply.github.com>
2024-04-05 21:23:34 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,24 @@
+// Q16:
+// More Guests: You just found a bigger dinner table, so now more  space is available,
+// Think of three more guests to invite to dinner, start with your program for 
+// Exercise 15. Add a print statement to the end of your program informing people that
+// you found a bigger dinner a table,
+
+// Add one new guest to the begining of your arry.
+
+// Add one new guest to the middle of your arry. Use append() to Add one new guest
+// to the end of your list. print a new set of invitation message, one for each person
+// in your list.*/
+
+const guest: string[] = ["Ayesha","Aiman","Rubab","Farwa"];
+console.log('Due to some reosone ${guest1[1]} will not come on my dinner');
+guest[1] = "Hudaim";
+for (let i=0;i<guest.length;i++){
+    // console.log('Dear ${guest1[i]}! I would like to invite yuo for dinner on sunday.');
+}
+
+console.log('Good News! we have arrange a big dinner table so we invite 3 more guest\nThank you')
+
+guest.unshift("Fahad");
+guest.splice
+
